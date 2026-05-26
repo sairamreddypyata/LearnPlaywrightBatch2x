1823fd3bf723901533197eaf39b6f307d4b73260
sairam <sairamreddy0191.com>
2026-05-26 16:55:53 +0530

    Add chapter_03_Identifier_Literals

---
+++ chapter_03_Identifier_Literals/06_Identifier_Rules.js
@@ -0,0 +1,34 @@
+var $ = 8;
+var _a = 9;
+var v = 10;
+console.log($);
+console.log(_a);
+console.log(v);
+
+var _ = "5";
+var abc123 =11;
+console.log(abc123);
+console.log(_);
+
+// var 123 = 12; // Invalid identifier, cannot start with a number.
+// var 123abc = 12; // Invalid identifier, cannot start with a number.
+
+var Name = "promod";
+var name = "john";
+console.log(Name);
+console.log(name);
+
+// var pramod dutta = "hello"; // Invalid identifier, cannot contain spaces .
+
+var pramod_dutta = "hello";
+var pramod$dutta = "hii";
+var pramod1245= "welcome";
+console.log(pramod_dutta);
+console.log(pramod$dutta);
+console.log(pramod1245);
+
+// var pramod-dutta = "byee"; // Invalid identifier, cannot contain hyphens.
+
+
+
+
---
+++ chapter_03_Identifier_Literals/08_Comments.js
@@ -0,0 +1,18 @@
+//this is single line .thiw will be ignored .
+//this will not be executed .
+
+/*
+* This is multi line comment.
+* author : sairam
+* date : 14-02-2024
+* This will not be executed.
+*/
+
+
+/** 
+* This is multi line comment.
+* author : sairam
+* date : 14-02-2024
+* This will not be executed.
+**/
+
